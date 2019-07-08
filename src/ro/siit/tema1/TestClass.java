deff424bb7048926eec96271c73f49ee370f7b56
Georgiana Simpetreanu <georgiana_simpetreanu@yahoo.com>
2019-07-08 20:08:53 +0300

    test for changes

--- src/ro/siit/tema1/TestClass.java
+++ src/ro/siit/tema1/TestClass.java
@@ -4,6 +4,9 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("This is the test");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Print");
+        }
     }
 
 }
