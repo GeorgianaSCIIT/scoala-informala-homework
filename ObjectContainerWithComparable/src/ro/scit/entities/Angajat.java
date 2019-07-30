e7ca373080e1af072e39a30acb19517747e8e5bd
Georgiana Simpetreanu <georgiana_simpetreanu@yahoo.com>
2019-07-30 23:14:46 +0300

    Object containers homework

---
+++ ObjectContainerWithComparable/src/ro/scit/entities/Angajat.java
@@ -0,0 +1,13 @@
+package ro.scit.entities;
+
+/**
+ * @author Georgiana
+ * child class
+ */
+
+public class Angajat extends Person {
+
+    public Angajat(String name, int age) {
+        super(name, age);
+    }
+}
---
+++ ObjectContainerWithComparable/src/ro/scit/entities/Student.java
@@ -0,0 +1,13 @@
+package ro.scit.entities;
+
+/**
+ * @author Georgiana
+ * child class
+ */
+
+public class Student extends Person {
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+}
